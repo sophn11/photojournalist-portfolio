5fc2902c73e00fefe2b595e8ce8c660acc658fe0
sophn11 <sophianakpil@gmail.com>
2016-10-27 00:50:40 -0700

    creates show pages for exhibitions

---
+++ app/controllers/show_controller.rb
@@ -0,0 +1,126 @@
+class ShowController < ApplicationController
+
+  def peace1
+  end
+
+  def peace2
+  end
+
+  def peace3
+  end
+
+  def peace4
+  end
+
+  def peace6
+  end
+
+  def carlospizarro
+  end
+
+  def cartamagna
+  end
+
+  def dejaciondearmas
+  end
+
+  def elcartucho
+  end
+
+  def patrullando
+  end
+
+  def sinhorizonte
+  end
+
+  def sinpalabras
+  end
+
+  def aztecaboy
+  end
+
+  def boysandcows
+  end
+
+  def candle
+  end
+
+  def comida
+  end
+
+  def couple
+  end
+
+  def girls
+  end
+
+  def familia03
+  end
+
+  def kikayoga
+  end
+
+  def kika
+  end
+
+  def mariachi
+  end
+
+  def skull
+  end
+
+  def businesswarbusiness
+  end
+
+  def err
+  end
+
+  def estratagia
+  end
+
+  def negociaciones
+  end
+
+  def negocianes
+  end
+
+  def elcartucho92
+  end
+
+  def laflaca
+  end
+
+  def lapastora
+  end
+
+  def elcartucho
+  end
+
+  def buga89
+  end
+
+  def buga1
+  end
+
+  def circulodeminiusis
+  end
+
+  def encampana
+  end
+
+  def legandodemexico
+  end
+
+  def rueda
+  end
+
+  def santamarta
+  end
+
+  def santander
+  end
+
+  def velorio
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,56 @@ Rails.application.routes.draw do
   get '/recyclers', to: 'exhibits#recyclers'
   get '/cesar', to: 'exhibits#cesar'
 
+  get '/peace1', to: 'show#peace1'
+  get '/peace2', to: 'show#peace2'
+  get '/peace3', to: 'show#peace3'
+  get '/peace4', to: 'show#peace4'
+  get '/peace6', to: 'show#peace6'
+
+  get '/carlospizarro', to: 'show#carlospizarro'
+  get '/cartamagna', to: 'show#cartamagna'
+  get '/dejaciondearmas', to: 'show#dejaciondearmas'
+  get '/elcartucho', to: 'show#elcartucho'
+  get '/patrullando', to: 'show#patrullando'
+  get '/sinhorizonte', to: 'show#sinhorizonte'
+  get '/sinpalabras', to: 'show#sinpalabras'
+
+  get '/aztecaboy', to: 'show#aztecaboy'
+  get '/boysandcows', to: 'show#boysandcows'
+  get '/candle', to: 'show#candle'
+  get '/comida', to: 'show#comida'
+  get '/couple', to: 'show#couple'
+  get '/girls', to: 'show#girls'
+  get '/familia03', to: 'show#familia03'
+  get '/kikayoga', to: 'show#kikayoga'
+  get '/kika', to: 'show#kika'
+  get '/mariachi', to: 'show#mariachi'
+  get '/skull', to: 'show#skull'
+
+  get '/businesswarbusiness', to: 'show#businesswarbusiness'
+  get '/err', to: 'show#err'
+  get '/estratagia', to: 'show#estratagia'
+  get '/negociaciones', to: 'show#negociaciones'
+  get '/negocianes', to: 'show#negocianes'
+
+  get '/elcartucho92', to: 'show#elcartucho92'
+  get '/laflaca', to: 'show#laflaca'
+  get '/lapastora', to: 'show#lapastora'
+  get '/elcartucho', to: 'show#elcartucho'
+
+  get '/buga89', to: 'show#buga89'
+  get '/buga1', to: 'show#buga1'
+  get '/circulodeminiusis', to: 'show#circulodeminiusis'
+  get '/encampana', to: 'show#encampana'
+  get '/legandodemexico', to: 'show#legandodemexico'
+  get '/rueda', to: 'show#rueda'
+  get '/santamarta', to: 'show#santamarta'
+  get '/santander', to: 'show#santander'
+  get '/velorio', to: 'show#velorio'
+
+
+
+
+
 
 end
